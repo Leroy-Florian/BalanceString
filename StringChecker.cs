7f69875427db5b230247d36ee57e756da0e3b6c4
hijoxx <mr.leroyflorian@gmail.com>
2023-06-09 11:58:22 +0200

    fix:false positive  after end of comments

---
+++ StringChecker.cs
@@ -0,0 +1,84 @@
+﻿namespace ConsoleApp1;
+
+public static class StringChecker
+{
+    private static readonly Dictionary<char, char> SymbolsPairs = new Dictionary<char, char>()
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+    };
+
+    public static bool IsBalanced(string str)
+    {
+        var openingSymbolsStack = new Stack<char>();
+        var isCurrentlyCommented = false;
+
+        for (var i = 0; i < str.Length; i++)
+        {
+            var symbol = str[i];
+
+            if (!isCurrentlyCommented)
+            {
+                if (IsOpeningComments(symbol, str, i))
+                {
+                    isCurrentlyCommented = true;
+                    i++;
+                }
+                else if (IsOpeningSymbol(symbol))
+                {
+                    openingSymbolsStack.Push(symbol);
+                }
+                else if (IsClosingSymbol(symbol))
+                {
+                    if (IsOpeningStackEmpty(openingSymbolsStack) ||
+                        !AreSymbolsMatching(openingSymbolsStack.Pop(), symbol))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                if (IsClosingComments(symbol, str, i))
+                {
+                    isCurrentlyCommented = false;
+                    i++;
+                }
+            }
+        }
+
+        return IsOpeningStackEmpty(openingSymbolsStack) && !isCurrentlyCommented;
+    }
+
+
+    private static bool IsOpeningSymbol(char symbol)
+    {
+        return SymbolsPairs.ContainsKey(symbol);
+    }
+
+    private static bool IsClosingSymbol(char symbol)
+    {
+        return SymbolsPairs.ContainsValue(symbol);
+    }
+
+    private static bool IsOpeningComments(char symbol, string str, int index)
+    {
+        return symbol.ToString() == "/" && index < str.Length - 1 && str.Substring(index + 1, 1) == "*";
+    }
+
+    private static bool IsClosingComments(char symbol, string str, int index)
+    {
+        return symbol.ToString() == "*" && index < str.Length - 1 && str.Substring(index + 1, 1) == "/";
+    }
+
+    private static bool IsOpeningStackEmpty(Stack<char> stack)
+    {
+        return stack.Count == 0;
+    }
+
+    private static bool AreSymbolsMatching(char opening, char closing)
+    {
+        return SymbolsPairs[opening] == closing;
+    }
+}
---
+++ UseCasesTests.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1;
+
+public class UseCaseTests
+{
+    [Test]
+    public void SimpleBalancedString()
+    {
+        const string input = "( { [ () [] ] } )";
+        const bool expected = true;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void BalancedStringWithCharacters()
+    {
+        const string input = "(a{b[c(d)e[f]g]h}i)";
+        const bool expected = true;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void UnbalancedString()
+    {
+        const string input = "( ]";
+        const bool expected = false;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void SymbolsPairsButInWrongOrder()
+    {
+        const string input = ")(";
+        const bool expected = false;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void SymbolsPairsButItsEntangle()
+    {
+        const string input = "( [ ) ]";
+        const bool expected = false;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void SymbolsAreCommented()
+    {
+        const string input = "{ /* ) */ }";
+        const bool expected = true;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void SymbolsArNotCorrectlyMatchAfterComments() // Old False Positive
+    {
+        const string input = "{ /* ) ( */ )}";
+        const bool expected = false;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+    
+    [Test]
+    public void MultipleSymbolsAreCommented()
+    {
+        const string input = "{ /*/ ) */ }";
+        const bool expected = true;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void WeirdString()
+    {
+        const string input = "X/**";
+        const bool expected = false;
+
+        var result = StringChecker.IsBalanced(input);
+        Assert.AreEqual(expected, result);
+    }
+}
+
